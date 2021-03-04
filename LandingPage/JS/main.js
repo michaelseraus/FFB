eb76f887545c0b9b7b19eac2c18305d97cf139e8
Michael <mseraus@gmail.com>
2021-03-04 15:52:25 +0100

    finished landing page

---
+++ LandingPage/JS/main.js
@@ -0,0 +1,6 @@
+function menuToggle() {
+  document.getElementById("myDropdown").classList.toggle("show");
+  document.getElementById("line1").classList.toggle("animate-line1");
+  document.getElementById("line2").classList.toggle("animate-line2");
+  document.getElementById("line3").classList.toggle("animate-line3");
+}
